f2521ef4090d684f1d61a03e83f129b38237a855
pritz1 <prithvichauhan.963@gmail.com>
2019-10-11 18:25:25 +0530

    Search option in MGR RCPA Rx QTY -> Rx Qty/Wk -prithvi

--- app/src/main/java/com/eis/dailycallregister/Activity/MgrRcpaDrList.java
+++ app/src/main/java/com/eis/dailycallregister/Activity/MgrRcpaDrList.java
@@ -1,26 +1,26 @@
 package com.eis.dailycallregister.Activity;
 
 import android.app.ActivityOptions;
-import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.support.annotation.NonNull;
-import android.support.constraint.ConstraintLayout;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
 import android.text.Html;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
-import android.widget.SearchView;
 
 
 import com.eis.dailycallregister.Api.RetrofitClient;
@@ -30,6 +30,7 @@ import com.eis.dailycallregister.Pojo.MgrRcpaDrRes;
 import com.eis.dailycallregister.Pojo.RcpadrListItem;
 import com.eis.dailycallregister.R;
 
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -37,12 +38,12 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class MgrRcpaDrList extends AppCompatActivity {
+public class MgrRcpaDrList extends AppCompatActivity implements SearchView.OnQueryTextListener { //by prithvi SearchView.OnQueryTextListener
 
      RecyclerView drlistrecvw;
      ViewDialog progressDialoge;
     RelativeLayout nsv;
-    SearchView searchview;
+
 
     LinearLayout ll1;
     TextView hqname;
@@ -50,6 +51,7 @@ public class MgrRcpaDrList extends AppCompatActivity {
     String hname,netid;
 
     public List<RcpadrListItem> drlist= new ArrayList<>();
+    public List<RcpadrListItem> drlistcopy = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -74,8 +76,8 @@ public class MgrRcpaDrList extends AppCompatActivity {
             hqname.setText( headqname );
             ll1.setVisibility(View.VISIBLE);
         }
-
 */
+
         callApi();
 
        /* if(searchview!=null)
@@ -126,6 +128,7 @@ public class MgrRcpaDrList extends AppCompatActivity {
             //  Log.d("response 2",res.toString());
 
               drlist =  res.getDrlist();
+              drlistcopy = new ArrayList<>(drlist); //prithvi 10/10/2019 - to include search functionality.
           //   Log.d("hqpsrlist 1",drlist.toString());
               drlistrecvw.setVisibility(View.VISIBLE);
               drlistrecvw.getAdapter().notifyDataSetChanged();
@@ -218,4 +221,60 @@ public class MgrRcpaDrList extends AppCompatActivity {
              finish();
           MgrRcpaDrList.this.overridePendingTransition(R.anim.trans_right_in, R.anim.trans_right_out);
     }
+//prithvi -> 10/10/2019 for search functionality
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //MenuInflater inflater = getMenuInflater();
+        getMenuInflater().inflate(R.menu.search_menu, menu);
+        MenuItem menuItem = menu.findItem(R.id.search);
+
+        android.support.v7.widget.SearchView searchView = (android.support.v7.widget.SearchView) menuItem.getActionView();
+        searchView.setQueryHint("Search...");
+        LinearLayout first = (LinearLayout)searchView.getChildAt(0);
+        LinearLayout second = (LinearLayout)first.getChildAt(2);
+        LinearLayout third = (LinearLayout)second.getChildAt(1);
+        SearchView.SearchAutoComplete autoComplete = (SearchView.SearchAutoComplete)third.getChildAt(0);
+        autoComplete.setHintTextColor(getResources().getColor(R.color.charcoal));
+        autoComplete.setTextColor(getResources().getColor(R.color.charcoal));
+
+        searchView.setOnQueryTextListener(this);
+
+        ImageView searchClose = (ImageView) searchView.findViewById(android.support.v7.appcompat.R.id.search_close_btn);
+        searchClose.setImageResource(R.drawable.ic_clear_24dp);
+
+        //Field mCollapseIcon = searchView.get;
+
+        return true;
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        return false;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String newText) {
+
+        String userInput = newText.toLowerCase();
+        List<RcpadrListItem> newList = new ArrayList<>();
+        drlist = new ArrayList<>();
+
+        if(newText != null && !newText.equalsIgnoreCase("")) {
+            for (RcpadrListItem obj : drlistcopy) {
+                if (obj.getDrname().toLowerCase().contains(userInput)) {
+                    newList.add(obj);
+                }
+            }
+
+            drlist.addAll(newList);
+        }else{
+
+            drlist.addAll(drlistcopy);
+        }
+
+        drlistrecvw.getAdapter().notifyDataSetChanged();
+
+        return true;
+    }
+
 }
